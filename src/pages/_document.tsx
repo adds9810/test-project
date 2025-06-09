af3dd0e43c54054cc0324e1e2dc3595d3a6e6da5
김지혜 <ria9810@gmail.com>
2025-06-09 16:54:48 +0900

    chore: 포트폴리오 제출용 meta 태그 구성 정리

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,15 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:site_name" content="Saju Visualizer" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <meta property="og:image" content="/images/og_1200x630.png" />
+        <meta
+          property="og:image:alt"
+          content="운세박사 - 청담동 점짐, 프리미엄 사주, 신년운세, 오늘의 운세"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <body className="antialiased">
         <Main />
